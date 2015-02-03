489a08b08df0ce864ac2fa095cf3f81616dd0194
Jennifer Grayburn <jng3au@virginia.edu>
2015-02-03 05:03:45 -0500

    add final homework page

---
+++ variable.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Variable Assignment</title>
+	<link rel="stylesheet" href="variable.css">
+</head>
+
+<body id="body">	
+	<?php
+	echo "Jen Grayburn is " . $_GET['adjective']; 
+
+	?>	
+
+	<p><a href="?adjective=even more awesome!"> Click for more awesome! </a></p>
+
